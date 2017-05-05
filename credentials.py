b33f7806240328b988adb5c27a8e15e56c10d21d
Christoph Matthies <christoph.matthies@gmail.com>
2017-05-05 15:40:20 +0200

    Read credentials from env vars

--- credentials.py
+++ credentials.py
@@ -1,4 +1,6 @@
-APP_KEY = ''
-APP_SECRET = ''
-OAUTH_TOKEN = ''
-OAUTH_TOKEN_SECRET = ''
+import os
+
+APP_KEY = os.environ['APP_KEY']
+APP_SECRET = os.environ['APP_SECRET']
+OAUTH_TOKEN = os.environ['OAUTH_TOKEN']
+OAUTH_TOKEN_SECRET = os.environ['OAUTH_TOKEN_SECRET']
