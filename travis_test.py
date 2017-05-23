62e7fd64fcd7d921846bf38b9362fb63fe465127
Arian Treffer <derari@web.de>
2017-05-23 10:50:24 +0200

    tasks

---
+++ travis_test.py
@@ -0,0 +1,49 @@
+# https://docs.python.org/2/library/unittest.html
+import unittest
+
+from tweet_text import idle_text, reply
+from travis.submit_issue import submit_issue
+from sys import exit
+
+# The basic building blocks of unit testing are test cases
+# single scenarios that must be set up and checked for correctness.
+# In unittest, test cases are represented by instances of unittest's TestCase class.
+# To make your own test cases you must write subclasses of TestCase.
+
+class TestTasks(unittest.TestCase):
+
+	def __init__(self, *args, **kwargs):
+		super(TestTasks, self).__init__(*args, **kwargs)
+		self.score = 0
+
+	def issue(self, title, body):
+		self.score = self.score + 1
+		self.title = title
+		self.body = body
+		
+	def error(self, message):
+		msg = self.body + "\n\n>" + message
+		print self.title
+		print msg
+		submit_issue(self.title, msg)
+		exit(1)
+		
+	def expect_contains(self, what, string, substr):
+		if substr not in string:
+			self.error("Expected {0} to contain \"{1}\",  \nbut got \"{2}\".".format(what, substr, string))
+
+	def expect_no_error(self, error):
+		self.error("Got {0}".format(error))
+
+	def test_math1(self):
+		self.issue('The bot should be able to do simple math', 
+		"Given a tweet \"1+1\",  \nThen the bot's answer should contain \"2\".")
+		try:
+			response = reply("1+1")
+			self.expect_contains("response", response, "2")
+		except Exception as ex:
+			self.expect_no_error(ex)
+			
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
