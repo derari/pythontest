f1d59efd5c7f4e6cac439b7558b72c667ea5c9a3
Christoph Matthies <christoph.matthies@gmail.com>
2017-05-23 14:15:28 +0200

    Print errors

--- travis_test.py
+++ travis_test.py
@@ -16,7 +16,7 @@ class TestTasks:
 
     def __init__(self):
         self.score = 0
-        
+
     def run(self):
         self.test_math1()
         self.test_math2()
@@ -24,7 +24,7 @@ class TestTasks:
         self.test_mention()
         self.test_over9000()
         self.test_oh_rly()
-        
+
         self.test_pokemon1()
         self.test_pokemon2()
         self.test_pokemon3()
@@ -35,7 +35,7 @@ class TestTasks:
         self.test_pokemon8()
         self.test_pokemon9()
         self.test_poke_math1()
-        
+
         self.test_done()
 
     def issue(self, title, body):
@@ -43,15 +43,16 @@ class TestTasks:
         self.title = title
         self.body = body
         print title
-        
+
     def error(self, message):
         msg = self.body + "\n\n>" + message
         if os.environ.get('TRAVIS_REPO_SLUG') is not None:
             submit_issue(self.title, msg, self.score)
         else:
             print "ERROR"
+            print message
         exit(1)
-        
+
     def expect_contains(self, what, string, substr):
         if string is None:
             self.error("Expected {0} containing \"{1}\",  \nbut got `None`".format(what, substr))
@@ -60,15 +61,15 @@ class TestTasks:
 
     def expect_no_error(self, error):
         self.error("Failed, got exception:\n```\n{0}```".format(error))
-        
+
     def reply_to(self, tweet):
         return reply({'text': tweet, 'user': {'screen_name': 'TestUser'}})
 
     def test_math1(self):
         """Given a tweet '1+1'. Then the bot's answer should contain '2'"""
-        self.issue('The bot should be able to do simple math', 
+        self.issue('The bot should be able to do simple math',
         """
-Given a tweet \"1+1\",  
+Given a tweet \"1+1\",
 Then the bot's answer should contain \"2\".
 
 * To split the term into operands, you can use [`string.split('+')`](https://docs.python.org/2/library/stdtypes.html#str.split).
@@ -84,9 +85,9 @@ Then the bot's answer should contain \"2\".
 
     def test_math2(self):
         """Given a tweet '1+2'. Then the bot's answer should contain '3'"""
-        self.issue('The bot should be able to actually do maths, not relying on hard-coded values', 
+        self.issue('The bot should be able to actually do maths, not relying on hard-coded values',
         """
-Given a tweet \"1+2\",  
+Given a tweet \"1+2\",
 Then the bot's answer should contain \"3\".
         """)
         try:
@@ -99,9 +100,9 @@ Then the bot's answer should contain \"3\".
 
     def test_math3(self):
         """Given a tweet '1999+1'. Then the bot's answer should contain '2000'"""
-        self.issue('The bot should be able to do maths with large numbers', 
+        self.issue('The bot should be able to do maths with large numbers',
         """
-Given a tweet \"1999+1\",  
+Given a tweet \"1999+1\",
 Then the bot's answer should contain \"2000\".
         """)
         try:
@@ -114,9 +115,9 @@ Then the bot's answer should contain \"2000\".
 
     def test_mention(self):
         """Given a tweet '@Bot 1+1'. Then the bot's answer should contain 2"""
-        self.issue('The bot should ignore the @mention', 
+        self.issue('The bot should ignore the @mention',
         """
-Given a tweet \"@Bot 1+1\",  
+Given a tweet \"@Bot 1+1\",
 Then the bot's answer should contain \"2\".
 
 * To check if a string starts with '@', you can use [`string.startswith('@')`](https://docs.python.org/2/library/stdtypes.html#str.startswith).
@@ -133,7 +134,7 @@ Then the bot's answer should contain \"2\".
     def test_over9000(self):
         """Given a tweet '@Bot 9000+1'. Then the bot's answer should contain '9001'
         and the string 'It's over nine thousand!'"""
-        self.issue('The bot should mention if numbers are large', 
+        self.issue('The bot should mention if numbers are large',
         """
 Given a tweet \"@Bot 9000+1\",
 Then the bot's answer should contain \"9001\" and \"It's over nine thousand!\".
@@ -149,7 +150,7 @@ Then the bot's answer should contain \"9001\" and \"It's over nine thousand!\".
 
     def test_oh_rly(self):
         """Given a tweet '@Bot oh rly?'. Then the bot's answer should contain 'YA RLY!'"""
-        self.issue('The bot should be confident', 
+        self.issue('The bot should be confident',
         """
 Given a tweet \"@Bot oh rly?\",
 Then the bot's answer should contain \"YA RLY!\"".
@@ -172,9 +173,9 @@ Then the bot's answer should contain \"YA RLY!\"".
 # Bulbasaur, Ivysaur, Venusaur, Charmander, Charmeleon, Charizard, Squirtle, Wartortle, Blastoise
 
     def test_pokemon1(self):
-        self.issue('The bot should know some Pokemon', 
+        self.issue('The bot should know some Pokemon',
         """
-Given a tweet \"Which Pokemon is #1\",  
+Given a tweet \"Which Pokemon is #1\",
 Then the bot's answer should contain \"Bulbasaur\".
 
 * It's enough if the bot knows Pokemon #1 to #9
@@ -188,9 +189,9 @@ Then the bot's answer should contain \"Bulbasaur\".
             self.expect_no_error(traceback.format_exc())
 
     def test_pokemon2(self):
-        self.issue('The bot should know Ivysaur', 
+        self.issue('The bot should know Ivysaur',
         """
-Given a tweet \"Which Pokemon is #2\",  
+Given a tweet \"Which Pokemon is #2\",
 Then the bot's answer should contain \"Ivysaur\".
 
 * It's enough if the bot knows Pokemon #1 to #9
@@ -202,11 +203,11 @@ Then the bot's answer should contain \"Ivysaur\".
             exit(1)
         except Exception as ex:
             self.expect_no_error(traceback.format_exc())
-            
+
     def test_pokemon3(self):
-        self.issue('The bot should know Venusaur', 
+        self.issue('The bot should know Venusaur',
         """
-Given a tweet \"Which Pokemon is #3\",  
+Given a tweet \"Which Pokemon is #3\",
 Then the bot's answer should contain \"Venusaur\".
 
 * It's enough if the bot knows Pokemon #1 to #9
@@ -218,11 +219,11 @@ Then the bot's answer should contain \"Venusaur\".
             exit(1)
         except Exception as ex:
             self.expect_no_error(traceback.format_exc())
-            
+
     def test_pokemon4(self):
-        self.issue('The bot should know Charmander', 
+        self.issue('The bot should know Charmander',
         """
-Given a tweet \"Which Pokemon is #4\",  
+Given a tweet \"Which Pokemon is #4\",
 Then the bot's answer should contain \"Charmander\".
 
 * It's enough if the bot knows Pokemon #1 to #9
@@ -234,11 +235,11 @@ Then the bot's answer should contain \"Charmander\".
             exit(1)
         except Exception as ex:
             self.expect_no_error(traceback.format_exc())
-            
+
     def test_pokemon5(self):
-        self.issue('The bot should know Charmeleon', 
+        self.issue('The bot should know Charmeleon',
         """
-Given a tweet \"Which Pokemon is #5\",  
+Given a tweet \"Which Pokemon is #5\",
 Then the bot's answer should contain \"Charmeleon\".
 
 * It's enough if the bot knows Pokemon #1 to #9
@@ -250,11 +251,11 @@ Then the bot's answer should contain \"Charmeleon\".
             exit(1)
         except Exception as ex:
             self.expect_no_error(traceback.format_exc())
-            
+
     def test_pokemon6(self):
-        self.issue('The bot should know Charizard', 
+        self.issue('The bot should know Charizard',
         """
-Given a tweet \"Which Pokemon is #6\",  
+Given a tweet \"Which Pokemon is #6\",
 Then the bot's answer should contain \"Charizard\".
 
 * It's enough if the bot knows Pokemon #1 to #9
@@ -266,11 +267,11 @@ Then the bot's answer should contain \"Charizard\".
             exit(1)
         except Exception as ex:
             self.expect_no_error(traceback.format_exc())
-            
+
     def test_pokemon7(self):
-        self.issue('The bot should know Squirtle', 
+        self.issue('The bot should know Squirtle',
         """
-Given a tweet \"Which Pokemon is #7\",  
+Given a tweet \"Which Pokemon is #7\",
 Then the bot's answer should contain \"Squirtle\".
 
 * It's enough if the bot knows Pokemon #1 to #9
@@ -282,11 +283,11 @@ Then the bot's answer should contain \"Squirtle\".
             exit(1)
         except Exception as ex:
             self.expect_no_error(traceback.format_exc())
-            
+
     def test_pokemon8(self):
-        self.issue('The bot should know Wartortle', 
+        self.issue('The bot should know Wartortle',
         """
-Given a tweet \"Which Pokemon is #8\",  
+Given a tweet \"Which Pokemon is #8\",
 Then the bot's answer should contain \"Wartortle\".
 
 * It's enough if the bot knows Pokemon #1 to #9
@@ -298,11 +299,11 @@ Then the bot's answer should contain \"Wartortle\".
             exit(1)
         except Exception as ex:
             self.expect_no_error(traceback.format_exc())
-            
+
     def test_pokemon9(self):
-        self.issue('The bot should know Blastoise', 
+        self.issue('The bot should know Blastoise',
         """
-Given a tweet \"Which Pokemon is #9\",  
+Given a tweet \"Which Pokemon is #9\",
 Then the bot's answer should contain \"Blastoise\".
 
 * It's enough if the bot knows Pokemon #1 to #9
@@ -316,9 +317,9 @@ Then the bot's answer should contain \"Blastoise\".
             self.expect_no_error(traceback.format_exc())
 
     def test_poke_math1(self):
-        self.issue('The bot should be able to do compute the Pokedex number', 
+        self.issue('The bot should be able to do compute the Pokedex number',
         """
-Given a tweet \"Which Pokemon is #1+2\",  
+Given a tweet \"Which Pokemon is #1+2\",
 Then the bot's answer should contain \"Venusaur\".
 
 * It's enough if the bot knows Pokemon #1 to #9
@@ -330,13 +331,13 @@ Then the bot's answer should contain \"Venusaur\".
             exit(1)
         except Exception as ex:
             self.expect_no_error(traceback.format_exc())
-            
-            
-            
+
+
+
     # insert all tests above this line ------------
     def test_done(self):
-        submit_issue('Congratulations, you have completed the exercise!', 
-            'To disable these notifications, remove the `before_install` and `after_success` hooks from your `.travis.yml` file.', 
+        submit_issue('Congratulations, you have completed the exercise!',
+            'To disable these notifications, remove the `before_install` and `after_success` hooks from your `.travis.yml` file.',
             self.score+1)
         exit(1)
 
